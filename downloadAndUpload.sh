57b504fb599cd40bdd679fef353ab5d8e1b00668
andrefisch <anfischl@gmail.com>
2020-11-19 16:53:41 -0500

    changed something stupid by accident

--- downloadAndUpload.sh
+++ downloadAndUpload.sh
@@ -17,7 +17,7 @@ curl 'https://member.usafencing.org/referees/search?name_member_id_search=1001&d
 sleep .1m
 curl 'https://member.usafencing.org/referees/search?name_member_id_search=1002&division_search=&rating_type=highestUsaEarnedRating&rating_is=%3D&rating_val=any&show%5B%5D=usa&sort=name&format=csv' --output referee3.csv
 python csvtojson.py
-node `pwd`
+# node `pwd`
 # Uncomment these lines to delete all csv and or json files if you want
 mkdir old
 # Clean out old folder
